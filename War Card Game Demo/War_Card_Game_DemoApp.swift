4c22d002c44e3b5f1e7a8bdc6fa0c0db5e0da721
Nirmal Baral <1761199+ncbaral@users.noreply.github.com>
2024-09-27 17:58:39 +0100

    first commit

---
+++ War Card Game Demo/War_Card_Game_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game_DemoApp.swift
+//  War Card Game Demo
+//
+//  Created by NirmalBaral on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
